a07bf7ef4340efbf94691a798bb1b37aa73982e3
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-03-18 01:22:18 -0700

    005_dictionary.py

---
+++ 005_dictionary.py
@@ -0,0 +1,23 @@
+student_info = {"name": "Bola Tinubu", "age":20,"grade":"c","subject":["math","physics","chemistry"], "address":{"city":"lagos", "country": "Nigeria"}}
+print(student_info["name"])
+print(student_info.get("age"))
+print(student_info["subject"])
+print(student_info["address"]["city"])
+student_info["grade"] = "A"
+print (student_info)
+student_info["GPA"] = 4.5
+print(student_info)
+student_info.update({"age":21, "school":"OAU"})
+print(student_info)
+del student_info["GPA"]
+print(student_info)
+age = student_info.pop("age")
+print(student_info)
+print(len(student_info))
+print(student_info.keys())
+print(student_info.values())
+print(student_info.items())
+for key in student_info:
+      print(key)
+for key, value in student_info.items():
+      print(key,value)
