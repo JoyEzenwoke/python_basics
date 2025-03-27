fbbde7aea263a99cdf596ceadbe8d52b2aed763c
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-03-27 14:37:33 -0700

    019_age_clarification.py

---
+++ 019_age_clarification.py
@@ -0,0 +1,10 @@
+age = int(input("enter age ;"))
+if age < 14:
+    print("you are a child")
+elif 14 <= age <= 19:
+    print("you are a teenager")
+elif 20 <= age <= 51:
+    print("you are an adult")
+else:
+    print("you are a senior citizen")
+          
